efb867e9052c26837f8462d757fadfcd66cea6b8
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-16 15:50:43 +0800

    Update Admire.js

--- themes/hexo/components/Admire.js
+++ themes/hexo/components/Admire.js
@@ -4,6 +4,6 @@ return (
         id="admire"
         src="https://patchwiki.biligame.com/images/umamusume/2/20/o9e5yn4nvsm4lf8zon22bin9l7rrauk.png" 
         alt="Admrire Vega"
-        />;
-        )
+        />
+        );
 }
