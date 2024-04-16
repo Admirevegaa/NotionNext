9aad956bd75202c8a2c527e9768f3bc4ecd9767e
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-16 16:21:11 +0800

    更新了漂浮组件

--- themes/hexo/components/Admire.js
+++ themes/hexo/components/Admire.js
@@ -1,9 +1,11 @@
-export default function Admire() {
-return (
-        <img
-        id="admire"
-        src="https://patchwiki.biligame.com/images/umamusume/2/20/o9e5yn4nvsm4lf8zon22bin9l7rrauk.png" 
-        alt="Admrire Vega"
-        />
-        );
+import Admire from "./Adpic";
+import ANIAdmire from "./float";
+
+export default function APP() {
+  return (
+    <>
+      <Admire />
+      <ANIAdmire />
+    </>
+  );
 }
---
+++ themes/hexo/components/Adpic.js
@@ -0,0 +1,8 @@
+export default function Admire() {
+  return (
+      <img
+        id="admire"
+        src="https://patchwiki.biligame.com/images/umamusume/0/08/7u6ud327zarqitc9i49ftudknxzw674.png"
+      />
+  );
+}
