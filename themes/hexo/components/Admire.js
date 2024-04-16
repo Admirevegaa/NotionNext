7f27ac2e11a9e7b5cc6196c5155578766e77b9d9
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-16 15:49:17 +0800

    Update Admire.js

--- themes/hexo/components/Admire.js
+++ themes/hexo/components/Admire.js
@@ -1,8 +1,9 @@
 export default function Admire() {
 return (
         <img
-            id=admire
-            src="https://patchwiki.biligame.com/images/umamusume/2/20/o9e5yn4nvsm4lf8zon22bin9l7rrauk.png" alt="Admrire Vega"
+        id="admire"
+        src="https://patchwiki.biligame.com/images/umamusume/2/20/o9e5yn4nvsm4lf8zon22bin9l7rrauk.png" 
+        alt="Admrire Vega"
         />;
         )
 }
