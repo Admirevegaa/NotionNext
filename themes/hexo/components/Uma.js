04e2e50de83864c1cee22af1839fbae2f73a1dde
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-16 16:54:52 +0800

    Update Uma.js

--- themes/hexo/components/Uma.js
+++ themes/hexo/components/Uma.js
@@ -1,11 +1,11 @@
 import Adpic from "./Adpic";
-//import ANIAdmire from "./float";
+import ANIAdmire from "./float";
 
 export default function Uma() {
   return (
     <>
       <Adpic />
-      //<ANIAdmire />
+      <ANIAdmire />
     </>
   );
 }
