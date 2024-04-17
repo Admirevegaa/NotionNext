29524e1c852a117085f1753cd5f3b500a22dfde4
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-17 13:02:15 +0800

    Update float.js

--- themes/hexo/components/float.js
+++ themes/hexo/components/float.js
@@ -3,7 +3,7 @@ export default function ANIAdmire() {
   
   let count = 0
   let flag = true//判断素材状态
-  let img: document.getElementById("admire")
+  let img= document.getElementById("admire")
   
   img.style.AnimationPlayState = "paused"//动画初始状态为停止
 
