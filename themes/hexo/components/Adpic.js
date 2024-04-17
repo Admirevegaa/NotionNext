faac909f25d2fda611e8817ffffcf4cd1cb6a871
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-17 16:07:11 +0800

    Update Adpic.js

--- themes/hexo/components/Adpic.js
+++ themes/hexo/components/Adpic.js
@@ -2,7 +2,7 @@ export default function Adpic() {
   return (
     <img
       id="admire"
-      src="https://patchwiki.biligame.com/images/umamusume/0/08/7u6ud327zarqitc9i49ftudknxzw674.png"
+      src="https://patchwiki.biligame.com/images/umamusume/2/20/o9e5yn4nvsm4lf8zon22bin9l7rrauk.png"
     />
   );
 }
