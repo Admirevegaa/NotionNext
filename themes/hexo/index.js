7bde4ab479f1d586b9f185b9450d452e709dbf2d
Admirevegaa <165777998+Admirevegaa@users.noreply.github.com>
2024-04-25 17:19:48 +0800

    Update index.js

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -32,7 +32,7 @@ import { Style } from './style'
 import replaceSearchResult from '@/components/Mark'
 import { siteConfig } from '@/lib/config'
 import dynamic from 'next/dynamic'
-import Uma from './components/Uma'
+import Admire from '/components/Admire'
 
 const AlgoliaSearchModal = dynamic(() => import('@/components/AlgoliaSearchModal'), { ssr: false })
 
@@ -99,7 +99,7 @@ const LayoutBase = props => {
 
             {/*马娘组件*/}
             <div>
-                <Uma />
+                <Admire />
             </div>
 
             {/* 主区块 */}
