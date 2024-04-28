86a09bd7600e438447285c6c4dada6babcfb4183
Admirevegaa <admirevega@126.com>
2024-04-28 10:21:50 +0800

    更新 Admire.js

--- themes/hexo/components/Admire.js
+++ themes/hexo/components/Admire.js
@@ -1,6 +1,5 @@
-
 import { useRef, useState } from 'react';
-
+import {motion} from 'framer-motion'
 
 let count = 0;
 let fakeaddress = 'p1.png'
@@ -8,7 +7,6 @@ let flag = true
 function Admire() {
   
   const imgRef = useRef(null)
-  //const [isClicked, setClicked] = useState(false)
   
  
   //点击切换
@@ -44,14 +42,53 @@ function Admire() {
         gap:30
       }}
       ></div>
-    
-        <img
-      onClick={handleChange}
-      ref={imgRef}
-      className='admire'
-      src={address}
-      alt=""
+    <motion.div
+      style={{
+        width:256,
+        height:256,
+        position:'fixed',
+        display:'block',
+        left:-5,
+        bottom:100,
+        borderRadius:'20px',
+        userSelect:'none',
+      }}
+      initial={{
+        rotateY:0,
+        x:0
+      }}
+      animate={{
+          y:flag ? [0,-20,0] : [0,-21,0],
+          rotateY:flag ? [0,180]:[0,540],
+          x:flag ? [0]:[0,1600],
+          transition:{
+            duration:0.5,
+            rotateY:{duration:1},
+            x:{duration:1.5},
+          },
+          
+          
+        }}
+      whileTap={{
+        rotateZ:720,
+        transition:{
+          rotateZ:{
+            repeat:Infinity,
+            duration:0.5,
+            repeatType:"loop",
+          }
+        }
+      }}        
+      >
+        <motion.img
+        layout
+        onClick={handleChange}
+        ref={imgRef}
+        className='admire'
+        src={address}
+        alt=""
     />
+    </motion.div>
     </div>
   );
 }
